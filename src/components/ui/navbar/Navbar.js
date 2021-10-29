eb16a88d169f1013ec70998bd9aa4dc9af78f365
jsemalvarez <jsemalvarez@gmail.com>
2021-10-28 23:44:38 -0300

    rutas en navbar

--- src/components/ui/navbar/Navbar.js
+++ src/components/ui/navbar/Navbar.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
+
+    const { name } = useSelector(state => state.auth)
     return (
-        <div>
+        <div className="navbar">
             <h2>Navbar</h2>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/dashboard/user"> { name }</Link>
+                </li>
+                <li>              
+                    <button>Salir</button>
+                </li>
+          </ul>
         </div>
     )
 }
